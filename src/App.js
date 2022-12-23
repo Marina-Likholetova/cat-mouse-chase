cd8d209768af16d9637bc39eec1438721ad2c5d6
Marina <likholetova.marina@gmail.com>
2022-12-23 18:09:51 +0200

    Initial app components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import mouse from "./assets/mouse.svg";
+import "./App.css";
+import imagePaths from "./utils/getImagePaths";
+import getRandom from "./utils/getRandom";
+import generateId from "./utils/generateId";
+import getCirclePosition from "./utils/getCirclePosition";
+import Container from "./components/Container/Container";
+import Cat from "./components/Cat/Cat";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cats: [],
+        };
+    }
+
+    componentDidMount() {
+      this.setState({
+        cats: [...Array(getRandom(1, 10)).keys()].map((_, i) => ({
+          img: imagePaths[getRandom(0, imagePaths.length - 1)],
+          id: generateId(),
+          delay: (i + 1) * 1000
+        }))
+      });
+    }
+
+    render() {
+        const { cats } = this.state;
+        const catsLength = cats.length;
+
+        return (
+            <div className="App">
+                <Container logo={mouse}>
+                    {(coords) => (
+                        <>
+                            {cats?.map((cat, i) => (
+                                <Cat
+                                    coords={getCirclePosition(coords, i, catsLength)}
+                                    img={cat.img}
+                                    delay={cat.delay}
+                                    key={cat.id}
+                                />
+                            ))}
+                        </>
+                    )}
+                </Container>
+            </div>
+        );
+    }
 }
-
-export default App;
